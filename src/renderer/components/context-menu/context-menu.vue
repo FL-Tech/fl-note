5f36ab091aea242afc60190e25d98ac44bc5dd8d
zhangzhenfei <289556866@qq.com>
2018-08-05 21:11:44 +0800

    优化弹出框样式

--- src/renderer/components/context-menu/context-menu.vue
+++ src/renderer/components/context-menu/context-menu.vue
@@ -23,10 +23,8 @@ export default {
 
 <style lang="scss" scoped>
 .context-menu {
+  @include POPOVER;
   position: fixed;
-  background-color: rgba(38, 50, 56, 0.9);
-  border-radius: 5px;
-  box-shadow: 0 0 5px 1px rgba(0, 0, 0, 0.1);
   padding: 5px 0;
   z-index: 3;
 }
