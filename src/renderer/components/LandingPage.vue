6d984ea6ba682205107a180eb71c87fe27417d6e
zhangzhenfei <289556866@qq.com>
2018-08-12 23:40:20 +0800

    测试提交

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -6,7 +6,7 @@
     <main>
       <div class="left-side">
         <span class="title">
-          Welcome to your new!
+          Welcome to your new project!
         </span>
         <system-information></system-information>
       </div>
